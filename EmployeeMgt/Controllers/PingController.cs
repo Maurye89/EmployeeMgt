ed1875b98315e502c8a0e5371ab4f6d4ad517892
Abhishek kumar <Abhishek kumar@ATHARVPC>
2025-12-25 21:35:23 +0530

    Add controller to check

---
+++ EmployeeMgt/Controllers/PingController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeMgt.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PingController : ControllerBase
+    {
+        [HttpGet]
+        [Route("Health")]
+        public IActionResult Ping()
+        {
+            return Ok("Pong");
+        }
+    }
+}
